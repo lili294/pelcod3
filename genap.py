61bf708cba767be89da8403ea7ce4735c66d28ff
lili294 <143860033+lili294@users.noreply.github.com>
2023-09-04 19:03:43 +0700

    Add files via upload
    program upload:
    - beam volume
    - volume of tube
    - volume of a sphere

---
+++ genap.py
@@ -0,0 +1,32 @@
+print("nama : sholihatul muyassaroh")
+print("NIM : 230441100012")
+print("ploating: A")
+
+print(" ")
+
+print("program menghitung volume balok")
+print("rumus volume balok: p x l x t")
+p =int(input("masukan panjang balok = "))
+l =int(input("masukan lebar balok= "))
+t =int(input("masukan tinggi balok= "))
+volume_balok =str("jadi, volume balok =")
+print("jadi, volume balok =", p*l*t)
+
+print(" ")
+
+print("program menghitung volume tabung")
+print("rumus volume tabung: phi x r x r x tinggi")
+r =int(input("masukan jari-jari tabung = "))
+t =int(input("masukan tinggi tabung = "))
+phi = 22/7
+volume = phi*r*r*t
+print("jadi, volume tabung = ", volume)
+
+print(" ")
+
+print("program menghitung volume bola")
+print("rumus volume bola: 4/3 x phi x r x r x r")
+r =int(input("masukan jari-jari bola = "))
+phi = 22/7
+volume = 4/3*phi*r*r*r
+print("jadi, volume tabung = ",volume)
